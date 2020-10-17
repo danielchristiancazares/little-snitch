412a5993ffac41819088f137d1fbe87222c60b0c
Daniel Cazares <dcazares@skechers.com>
2020-10-17 11:57:49 -0700

    Update generate file. Combine all lists.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Host Files Used
+## SomeoneWhoCares List
+https://someonewhocares.org/hosts/zero/hosts
+## MVPS List
+https://winhelp2002.mvps.org/hosts.txt
+##Steven Black List
+https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
--- generate.sh
+++ generate.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-cat blacklist.txt | grep --extended-regexp '^[^#].*$' > blacklist2.txt
-sed 's/#.*//g' blacklist2.txt > blacklist.txt
-sed 's/ //g' blacklist.txt > blacklist2.txt
-cat blacklist2.txt > blacklist.txt
-rev blacklist2.txt | sort -nsd
-rev blacklist.txt | sort -nd > blacklist2.txt
-cat blacklist.txt | sort --field-separator='.' -d -k3,1 > blacklist2.txt
+wget https://winhelp2002.mvps.org/hosts.txt -O mvps.txt
+
+grep --extended-regexp '^0.0.0.0 .*[^0-9]$' mvps.txt| sed '/:/d' | sed 's/\t/ /g' | tr -s ' \n' | sed 's/#.*//g' | cut -d' ' -f2- | sed 's/ //g' | sort > mvps.tmp
+
+wget https://someonewhocares.org/hosts/zero/hosts -O someonewhocares.txt
+
+grep --extended-regexp '^0.0.0.0 .*[^0-9]$' someonewhocares.txt| sed '/:/d' | sed 's/\t/ /g' | tr -s ' \n' | sed 's/#.*//g' | cut -d' ' -f2- | sed 's/ //g' | sort > someonewhocares.tmp
+
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -O stevenblacklist.txt
+
+grep --extended-regexp '^0.0.0.0 .*[^0-9]$' stevenblacklist.txt | sed '/:/d' | sed 's/\t/ /g' | tr -s ' \n' | sed 's/#.*//g' | cut -d' ' -f2- | sed 's/ //g' | sort > stevenblacklist.tmp
+
+rm -rf combined.tmp
+
+cat mvps.tmp >> combined.tmp
+
+cat someonewhocares.tmp >> combined.tmp
+
+cat stevenblacklist.tmp >> combined.tmp
+
+# Daniel Blacklist
+
+echo -n '{"name":"Daniel's Combo Blacklist","description":"Daniel's Combo Blacklist","rules":[{"action":"deny","process":"any","remote-hosts": ["' > blacklist.tmp
+
+uniq -i combined.tmp | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/","/g' | tr -d '\n' >> blacklist.tmp
+
+echo -n '"]}]}' >> blacklist.tmp
+
+cat blacklist.tmp | tr -d '\n' >> blacklist.lsrules
+
+exit 0
