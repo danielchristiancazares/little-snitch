f196b2b89464f5e3b08173befa4579510c7479e5
Daniel Cazares <dcazares@skechers.com>
2020-10-17 14:46:42 -0700

    Add KAD hosts list.

--- generate.sh
+++ generate.sh
@@ -5,16 +5,22 @@ wget https://someonewhocares.org/hosts/zero/hosts -O someonewhocares.txt
 
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -O stevenblacklist.txt
 
+wget https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts -O add2o7.txt
+
+wget https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts_without_controversies.txt -O KAD.txt
+
 # Try later to group sed with multiple commands. sed -e '/:/d' 's/\t/ /g' ... etc
+grep --extended-regexp '^0.0.0.0 .*[^0-9]$' KAD.txt             | sed -e 's/\n/ /g' -e '/:/d' | tr -dc '[[:print:]\n]' | tr -s ' \n' | sed -e 's/#.*//g' | cut -d' ' -f2- | sed -e 's/ //g' > KAD.tmp
+
+grep --extended-regexp '^0.0.0.0 .*[^0-9]$' add2o7.txt          | sed -e 's/\n/ /g' -e '/:/d' | tr -dc '[[:print:]\n]' | tr -s ' \n' | sed -e 's/#.*//g' | cut -d' ' -f2- | sed -e 's/ //g' > add2o7.tmp
+
 grep --extended-regexp '^0.0.0.0 .*[^0-9]$' mvps.txt            | sed -e 's/\n/ /g' -e '/:/d' | tr -dc '[[:print:]\n]' | tr -s ' \n' | sed -e 's/#.*//g' | cut -d' ' -f2- | sed -e 's/ //g' > mvps.tmp
 
 grep --extended-regexp '^0.0.0.0 .*[^0-9]$' someonewhocares.txt | sed -e 's/\n/ /g' -e '/:/d' | tr -dc '[[:print:]\n]' | tr -s ' \n' | sed -e 's/#.*//g' | cut -d' ' -f2- | sed -e 's/ //g' > someonewhocares.tmp
 
 grep --extended-regexp '^0.0.0.0 .*[^0-9]$' stevenblacklist.txt | sed -e 's/\n/ /g' -e '/:/d' | tr -dc '[[:print:]\n]' | tr -s ' \n' | sed -e 's/#.*//g' | cut -d' ' -f2- | sed -e 's/ //g' > stevenblacklist.tmp
 
-# sed -i -e "s/\r//g" combined.tmp
-
-sort -t'\n' --ignore-case --ignore-nonprinting --ignore-leading-blanks --dictionary-order --unique --mergesort --output='combined.tmp' someonewhocares.tmp mvps.tmp stevenblacklist.tmp
+sort -t'\n' --ignore-case --ignore-nonprinting --ignore-leading-blanks --dictionary-order --unique --mergesort --output='combined.tmp' someonewhocares.tmp mvps.tmp stevenblacklist.tmp add2o7.tmp KAD.tmp
 
 # Daniel Blacklist
 
